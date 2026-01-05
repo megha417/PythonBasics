589fb653cb2a8c196613381233b3811172ae4e58
Shinde Megha <meghashinde725@gmail.com>
2026-01-05 20:13:13 +0530

    slight changes

--- engine_room.py
+++ engine_room.py
@@ -5,3 +5,10 @@ print(text.find('e'))
 print(text.count('e'))
 print(text.replace('e','z'))
 print(len(text))
+
+
+name = input('Enter your name: ')   # once this is run, type anything in the box
+print(name)
+print(type(name))
+age = int(input('Enter your age: '))  # once this is run, type a number in the box
+print(age)
