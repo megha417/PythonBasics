d85ebb940ff0a1755dc6f488191cc9ddd615f8b4
Shinde Megha <meghashinde725@gmail.com>
2026-01-05 20:32:54 +0530

    Added Datatypes program

---
+++ Datatypes.py
@@ -0,0 +1,24 @@
+# Let's store a string (string = any kind of text) in a variable. In the following example, 'dummy' is a variable which holds some text data (string) 
+# Use print() to print something (it's not necessary in google colab, but tag along with us here for now)
+
+dummy = "Hello world!"
+print(dummy)
+
+# check the data type of variable 'dummy'
+print(type(dummy))
+
+# explore other data type quickly before focusing our attention to strings
+
+num = 3
+another_num = 3.0
+print(type(num))
+print(type(another_num))
+
+val = True
+print(type(val))
+
+# Let's just print the actual values 
+
+print(num)
+print(another_num)
+print(val)
